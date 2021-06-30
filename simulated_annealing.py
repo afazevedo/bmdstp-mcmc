1a7125db32ea1ef61a938fe58ec407bd8df01e2c
Amanda Azevedo <afazevedo29@gmail.com>
2021-06-30 15:58:05 -0300

    algoritmo funcional

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -11,45 +11,47 @@ def SA(eps, alpha, SAmax, T_initial, s):
     solucao_otima = f(s)
     iterT = 0 
     T = T_initial
-    aux = s
-    best_solution = aux
+    best_solution = nx.Graph.copy(s)
     while T > eps:
         while iterT < SAmax:
             iterT = iterT + 1
-            s_t = transition_neighbor(s)
             sol_atual = f(s)
+            s_t = transition_neighbor(s)
             sol_vizinho = f(s_t) 
+            # print("Solução atual: ", sol_atual, "Solução do vizinho: ", sol_vizinho)
             delta = sol_vizinho - sol_atual
-           
-            if delta <= 0:
+            if delta < 0:
                 s = s_t 
                 if sol_vizinho < solucao_otima:
-                    aux = s_t
-                    best_solution = aux
+                    # print("Entra aqui?")
+                    best_solution = nx.Graph.copy(s_t)
                     solucao_otima = sol_vizinho 
-
             else:
-                x = np.random.uniform(0,1)
+                x = np.random.uniform(0, 1)
                 boltzmann = np.exp(((-1)*delta)/T)
                 if x < boltzmann:
                     s = s_t
         T = T*alpha
         iterT = 0  
+    print("Solução ótima", solucao_otima)
     return best_solution, solucao_otima  
 
 # Leitura do grafo
-path = 'C:\\Users\\Wanderson\\OneDrive\\Documentos\\GitHub\\project_mcmc\\instances\\states_brazil.txt'
+path = 'D:\\mndzvd\\Documentos\\GitHub\\project_mcmc\\instances\\states_brazil.txt'
 n, C = readFiles(path)
 G = create_graph(n, C)
 
 # Solução inicial
-b_tree = nx.bfs_tree(G, 0, reverse=False, depth_limit=None, sort_neighbors=None)
+b_tree = nx.dfs_tree(G, 0)
 b_tree = b_tree.to_undirected()
+
+# nx.draw(b_tree, with_labels=True, font_weight='bold')
+# plt.show()
 
 # Precisão da temperatura final, taxa de resfriamento, 
 # número maximo de iterações por temperatura, temperatura inicial, solução inicial
 
-graph, sol = SA(0.001, 0.6, 10, 100, b_tree)
+graph, sol = SA(0.001, 0.8, 10, 100, b_tree)
 nx.draw(graph, with_labels=True, font_weight='bold')
 plt.show()
 print(f(graph), sol)
